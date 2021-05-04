18c21cece97579c88ec5265a0b4732af515a83f8
Your Name <you@example.com>
2021-05-04 19:35:03 +0600

    initional commit

---
+++ My_class.py
@@ -0,0 +1,21 @@
+class Phone:
+
+    # создаем атрибуты класса
+    name = "Note7"
+    company = "MI"
+    model = 2017
+
+    # создаем методы класса
+    def start(self):
+        print ("Включаем телефон")
+
+    def stop(self):
+        print ("выключаем телефон")
+
+
+
+
+Phone_a = Phone()
+
+
+Phone_b = Phone()
